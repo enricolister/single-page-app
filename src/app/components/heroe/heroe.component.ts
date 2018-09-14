4c7b486ccfeea38e37ff0369b9de3a0644a108ee
Enrico Lister <enricolister@hotmail.com>
2018-09-15 01:02:25 +0200

    - Created single Heroe pages and completed routing system

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, /*OnInit*/ } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styles: []
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent /*implements OnInit*/ {
 
-  constructor() { }
+  heroe: any = {};
+  router: Router;
 
-  ngOnInit() {
+  constructor( private activatedRoute: ActivatedRoute,
+               private _heroesService: HeroesService,
+               private _router: Router ) {
+
+    this.activatedRoute.params.subscribe( params => {
+      // console.log(params['id']);
+      this.heroe = this._heroesService.getHeroe(params['id']);
+      this.router = _router;
+      console.log(this.router.url); // questo solo per avere il log dell'url corrente nella console
+    });
   }
+
+ displayHouseImage() {
+    if (this.heroe['casa'] === 'DC') {
+      return 'assets/img/dc_comics.jpg';
+    } else {
+      return 'assets/img/marvel.jpg';
+  }
+ }
+
+ displayHouseAlt() {
+    if (this.heroe['casa'] === 'DC') {
+      return 'DC Comics';
+    } else {
+      return 'Marvel';
+  }
+ }
 
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -63,6 +63,10 @@ export class HeroesService {
     return this.heroes;
   }
 
+  getHeroe(id) {
+    return this.heroes[id];
+  }
+
 }
 
 export interface Heroe {
